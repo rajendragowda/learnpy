507ae58c813de9ceceb2f26f6cf1b8cc128a16b9
Rajendra Gowda <rajendra@Rajendras-iMac.local>
2018-03-24 20:13:03 +0530

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,79 @@
+# commit again
+# print('Hello World!')
+# print(1+4)
+# print(7*6)
+# print()
+# print('end"')
+# print("hello"+" world")
+# greeting = "Hello"
+# name = input("enter your name")
+# #commenting here
+# print(greeting + ' ' + name)
+
+# print("hey how are you \'lol\'\"es")
+#
+# splitString = """hi
+# this
+# script
+# is bakwas
+# """
+#
+# print(splitString)
+
+#
+# age = 24
+# name = 'raj'
+#
+# # print(age + name)
+# a = 13
+# b = 2
+# print(a + b)
+# print(a / b)
+# print(a % b)
+# print(a // b)
+# print(((2+3)*4)-6)
+# for i in range(1, a // b):
+#     print(i)
+
+# parrot ="abassdkfjsldjfs"
+# numbers = "1,2,3,4,5,6,7"
+# print(parrot)
+# print(parrot[3])
+# print(parrot[-3])
+# print(parrot[0:7])
+# print(parrot[7:])
+# print(parrot[-3:-1])
+# print(parrot[0:9:2])
+# print(numbers[0::2])
+# print (parrot *5)
+#
+# print("abaf" in parrot)
+
+# age = 2
+# print("mya age is " + str(age) + " years")
+#
+# print("mya age is {0} years".format(age))
+#
+# print ("my age is {0} or {1} or {2}".format(1,2,3))
+#
+#
+# for i in range(1,12):
+#     print("No. {0:2} squared is {1:<4} and cubed is {2:4}".format(i,i**2,i**3))
+#     print("pi value is {0:1.50}".format(22/7))
+
+
+# name = input("please enter your name")
+# age = int(input("please enter your age {0}".format(name)))
+# # print (age)
+#
+# if (age < 18) or (age > 32):
+#     print("you need to be inbetween 18-32")
+# else:
+#     print("please buy the tickets")
+
+dinner = ["water", "foo", "salad", "pork", "cucumber"]
+
+for items in dinner:
+    if items == "foo":
+        continue
+    print(items)
---
+++ Learn_Git.py
@@ -0,0 +1,7 @@
+print("hello World")
+
+numbers= "123jknfwjn1238ufndv9w8hfn3unf"
+for char in numbers:
+    if char in "0123456789":
+        print(char)
+        print("This is the version 2")
